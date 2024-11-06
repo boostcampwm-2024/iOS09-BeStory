a1567e70b4c6b7d483957262dcc66914ebbd5889
051198Hz <peeper_o_o@naver.com>
2024-11-06 23:40:01 +0900

    [Chore]: 파일 및 폴더 구조 변경

--- App/App/RootViewController.swift
+++ App/App/RootViewController.swift
@@ -6,10 +6,27 @@
 //
 
 import UIKit
+import Feature
 
 final class RootViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print(#function)
+        view.backgroundColor = .systemBackground
 	}
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let title = "iOS09 - 차은우원빈현빈장원영"
+        let users = [
+            "건우",
+            "지혜",
+            "석영"
+        ].map { InvitedUser(id: $0, name: $0, state: .connected) }
+        let groupInfoViewController = GroupInfoViewController(title: title, users: users)
+        addChild(groupInfoViewController)
+        view.addSubview(groupInfoViewController.view)
+        //didMove 메서드를 호출해야 ContinerVC.view.superView가 할당됨
+        groupInfoViewController.didMove(toParent: self)
+    }
 }
