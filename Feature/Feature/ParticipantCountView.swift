b37af9345ee1a3134c1f56f52e7bb373bed56a10
051198Hz <peeper_o_o@naver.com>
2024-11-06 23:22:40 +0900

    [Feat]: 그룹의 참가자 수를 보여주는 뷰 구현

---
+++ Feature/Feature/ParticipantCountView.swift
@@ -0,0 +1,62 @@
+//
+//  ParticipantCountView.swift
+//  Feature
+//
+//  Created by Yune gim on 11/6/24.
+//
+
+import SnapKit
+import UIKit
+
+class ParticipantCountView: UIView {
+    let groupImageView = UIImageView()
+    let countLabel = UILabel()
+    private var userCount: Int
+    
+    init(count: Int) {
+        userCount = count
+        super.init(frame: .zero)
+        setupViewHierarchies()
+        setupViewConstraints()
+        setupViewAttributes()
+    }
+    
+    convenience init() {
+        self.init(count: 0)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init with XIB not supported")
+    }
+    
+    public override var intrinsicContentSize: CGSize {
+        return CGSize(width: 40, height: 20)
+    }
+    func updateCount(to count: Int) {
+        userCount = count
+        countLabel.text = "\(count)"
+    }
+}
+
+private extension ParticipantCountView {
+    func setupViewHierarchies() {
+        addSubview(groupImageView)
+        addSubview(countLabel)
+    }
+    
+    func setupViewConstraints() {
+        groupImageView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+        }
+        countLabel.snp.makeConstraints {
+            $0.top.trailing.bottom.equalToSuperview()
+            $0.leading.equalTo(groupImageView.snp.trailing).offset(8)
+        }
+    }
+    
+    func setupViewAttributes() {
+        backgroundColor = .green
+        groupImageView.image = UIImage(systemName: "person.2.wave.2.fill")
+        countLabel.text = "\(userCount)"
+    }
+}
