8202fb961861ca4d6d246d520b8f47327d8b3e55
051198Hz <peeper_o_o@naver.com>
2024-11-07 14:03:45 +0900

    [FIX]: Feature 외 레이어 변경사항 제거

--- App/App/RootViewController.swift
+++ App/App/RootViewController.swift
@@ -5,24 +5,11 @@
 //  Created by jung on 11/4/24.
 //
 
-import Domain
 import UIKit
-import Feature
 
 final class RootViewController: UIViewController {
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		print(#function)
-        view.backgroundColor = .systemBackground
-	}
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        let groupInfoViewController = GroupInfoViewController()
-        addChild(groupInfoViewController)
-        view.addSubview(groupInfoViewController.view)
-        //didMove 메서드를 호출해야 ContinerVC.view.superView가 할당됨
-        groupInfoViewController.didMove(toParent: self)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
     }
 }
