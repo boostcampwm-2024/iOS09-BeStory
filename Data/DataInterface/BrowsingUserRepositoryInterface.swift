a6c4aa1caa0fa54d6b81334214a3779eeb6d4fe7
Jung SeokYoung <thrdud0423@gmail.com>
2024-11-07 15:27:28 +0900

    [CHORE]: Entity 모듈 추가

---
+++ Data/DataInterface/BrowsingUserRepositoryInterface.swift
@@ -0,0 +1,10 @@
+//
+//  BrowsingUserRepository.swift
+//  DataInterface
+//
+//  Created by jung on 11/7/24.
+//
+
+import Foundation
+
+
---
+++ Domain/Entity/User.swift
@@ -0,0 +1,10 @@
+//
+//  User.swift
+//  Entity
+//
+//  Created by jung on 11/7/24.
+//
+
+import Foundation
+
+public struct User { }
