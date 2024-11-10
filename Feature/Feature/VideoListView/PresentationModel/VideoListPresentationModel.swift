986c8161568b97b1ea3924a8b52db54a74692974
LURKS02 <thecrooks2@gmail.com>
2024-11-11 02:04:16 +0900

    [FEAT] 비디오 리스트를 보여줄 때 사용하는 Presentation Model 구현

---
+++ Feature/Feature/VideoListView/PresentationModel/VideoListPresentationModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoListPresentationModel.swift
+//  Feature
+//
+//  Created by 디해 on 11/7/24.
+//
+
+import Foundation
+
+public struct VideoListPresentationModel {
+    let title: String
+    let authorTitle: String
+    let thumbnailImage: Data
+    let duration: String
+    let date: String
+}
