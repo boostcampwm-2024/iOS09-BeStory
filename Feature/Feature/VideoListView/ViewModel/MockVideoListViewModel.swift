76b0180eb44ceb4de7f3929fdbacda51647c52f6
LURKS02 <thecrooks2@gmail.com>
2024-11-12 12:03:08 +0900

    [STYLE]: 배열을 나열하는 코드를 코드 컨벤션에 맞게 수정

--- Feature/Feature/VideoListView/ViewModel/MockVideoListViewModel.swift
+++ Feature/Feature/VideoListView/ViewModel/MockVideoListViewModel.swift
@@ -13,25 +13,33 @@ final public class MockVideoListViewModel: VideoListViewModel {
     public var videos: ReadOnlyPublisher<[VideoListItem]>
     
     private func load() {
-        videos.send([VideoListItem(title: "건우님이_차은우.avi",
-                                                authorTitle: "from 지혜",
-                                                thumbnailImage: Data.imageData() ?? Data(),
-                                                duration: "2:30",
-                                                date: "2024.07.15"),
-                     VideoListItem(title: "지혜님이_장원영.avi",
-                                                authorTitle: "from 건우",
-                                                thumbnailImage: Data.imageData() ?? Data(),
-                                                duration: "2:30",
-                                                date: "2024.07.15")])
+        videos.send(
+            [VideoListItem(
+                title: "건우님이_차은우.avi",
+                authorTitle: "from 지혜",
+                thumbnailImage: Data.imageData() ?? Data(),
+                duration: "2:30",
+                date: "2024.07.15"
+            ),
+             VideoListItem(
+                title: "지혜님이_장원영.avi",
+                authorTitle: "from 건우",
+                thumbnailImage: Data.imageData() ?? Data(),
+                duration: "2:30",
+                date: "2024.07.15"
+             )]
+        )
     }
     
     /// 테스트를 위해 Dummy Video를 추가합니다.
     private func appendDummyVideo() {
-        let dummyVideo = VideoListItem(title: "테스트_비디오.avi",
-                                       authorTitle: "from 지혜",
-                                       thumbnailImage: Data.imageData() ?? Data(),
-                                       duration: "1:30",
-                                       date: "2024.01.03")
+        let dummyVideo = VideoListItem(
+            title: "테스트_비디오.avi",
+            authorTitle: "from 지혜",
+            thumbnailImage: Data.imageData() ?? Data(),
+            duration: "1:30",
+            date: "2024.01.03"
+        )
         var currentVideos = videos.value
         currentVideos.append(dummyVideo)
         videos.send(currentVideos)
