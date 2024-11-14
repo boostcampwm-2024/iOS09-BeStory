4b2d7853cb133ec55b1d8acf36579e5e5f3469f8
Jung SeokYoung <thrdud0423@gmail.com>
2024-11-14 23:43:00 +0900

    [FEAT]: BrowsingRepository에 초대 및 수락/ 거절 로직 구현

--- Data/Data/BrowsingUserRepository.swift
+++ Data/Data/BrowsingUserRepository.swift
@@ -14,15 +14,13 @@ public final class BrowsingUserRepository: BrowsingUserRepositoryInterface {
 	private var cancellables: Set<AnyCancellable> = []
 	private let socketProvider: SocketProvidable
 	public let updatedBrowsingUser = PassthroughSubject<BrowsedUser, Never>()
-	
+	public let updatedInvitedUser = PassthroughSubject<InvitedUser, Never>()
+	public let invitationReceived = PassthroughSubject<BrowsedUser, Never>()
+
 	public init(socketProvider: SocketProvidable) {
 		self.socketProvider = socketProvider
 		
-		socketProvider.updatedPeer
-			.compactMap { [weak self] peer in
-				self?.mappingToBrowsingUser(peer) }
-			.subscribe(updatedBrowsingUser)
-			.store(in: &cancellables)
+		bind()
 	}
 }
 
@@ -33,17 +31,60 @@ public extension BrowsingUserRepository {
 			.compactMap { mappingToBrowsingUser($0) }
 	}
 	
-	func inviteUser(with id: String) { }
+	func inviteUser(with id: String) {
+		socketProvider.invite(peer: id)
+	}
+	
+	func acceptInvitation(from id: String) {
+		socketProvider.acceptInvitation()
+	}
+	
+	func rejectInvitation(from id: String) {
+		socketProvider.rejectInvitation()
+	}
 }
 
 // MARK: - Private Methods
 private extension BrowsingUserRepository {
+	func bind() {
+		socketProvider.updatedPeer
+			.compactMap { [weak self] peer in
+				self?.mappingToBrowsingUser(peer)
+			}
+			.subscribe(updatedBrowsingUser)
+			.store(in: &cancellables)
+		
+		socketProvider.updatedPeer
+			.compactMap { [weak self] peer in
+				self?.mappingToInvitedUser(peer)
+			}
+			.subscribe(updatedInvitedUser)
+			.store(in: &cancellables)
+		
+		socketProvider.invitationReceived
+			.compactMap { [weak self] peer in
+				self?.mappingToBrowsingUser(peer)
+			}
+			.subscribe(invitationReceived)
+			.store(in: &cancellables)
+	}
+	
 	func mappingToBrowsingUser(_ peer: SocketPeer) -> BrowsedUser? {
 		switch peer.state {
 			case .found:
 				return .init(id: peer.id, state: .found, name: peer.name)
 			case .lost:
 				return .init(id: peer.id, state: .lost, name: peer.name)
+			default: return nil
+		}
+	}
+	
+	func mappingToInvitedUser(_ peer: SocketPeer) -> InvitedUser? {
+		switch peer.state {
+			case .connected:
+				return .init(id: peer.id, state: .accept, name: peer.name)
+			case .disconnected:
+				return .init(id: peer.id, state: .reject, name: peer.name)
 			default: return nil
 		}
 	}
--- Domain/Interfaces/RepositoryInterface/BrowsingUserRepositoryInterface.swift
+++ Domain/Interfaces/RepositoryInterface/BrowsingUserRepositoryInterface.swift
@@ -11,9 +11,13 @@ import P2PSocket
 
 public protocol BrowsingUserRepositoryInterface {
 	var updatedBrowsingUser: PassthroughSubject<BrowsedUser, Never> { get }
-	
+	var updatedInvitedUser: PassthroughSubject<InvitedUser, Never> { get }
+	var invitationReceived: PassthroughSubject<BrowsedUser, Never> { get }
+
 	init(socketProvider: SocketProvidable)
 	
 	func fetchBrowsingUsers() -> [BrowsedUser]
 	func inviteUser(with id: String)
+	func acceptInvitation(from id: String)
+	func rejectInvitation(from id: String)
 }
