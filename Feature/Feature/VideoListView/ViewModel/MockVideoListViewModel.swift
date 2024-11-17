e2567b0251805d4d767940392739be0a571a2751
LURKS02 <thecrooks2@gmail.com>
2024-11-18 02:21:20 +0900

    [FEAT]: 동영상 url로 UI를 구성하기 위해 VideoListItem에 url 추가 및 로직 작업

--- Feature/Feature/VideoListView/ViewModel/MockVideoListViewModel.swift
+++ Feature/Feature/VideoListView/ViewModel/MockVideoListViewModel.swift
@@ -5,27 +5,12 @@
 //  Created by 디해 on 11/13/24.
 //
 
-import UIKit
+import AVFoundation
 import Combine
 
 /// 비디오 리스트를 테스트하기 위한 Mock View Model
 public final class MockVideoListViewModel {
-    private var videos: [VideoListItem] = [
-        VideoListItem(
-            title: "건우님이_차은우.avi",
-            authorTitle: "from 지혜",
-            thumbnailImage: Data.imageData() ?? Data(),
-            duration: "2:30",
-            date: "2024.07.15"
-        ),
-        VideoListItem(
-            title: "지혜님이_장원영.avi",
-            authorTitle: "from 건우",
-            thumbnailImage: Data.imageData() ?? Data(),
-            duration: "2:30",
-            date: "2024.07.15"
-        )
-    ]
+    private var videoItems: [VideoListItem] = []
     
     var output = PassthroughSubject<Output, Never>()
     var cancellables: Set<AnyCancellable> = []
@@ -41,12 +26,15 @@ extension MockVideoListViewModel: VideoListViewModel {
             switch input {
             case .viewDidLoad:
                 guard let self else { return }
-                output.send(.videoListDidChanged(videos: self.videos))
+                output.send(.videoListDidChanged(videos: self.videoItems))
                 
-            case .appendVideo:
-                self?.appendDummyVideo()
-                guard let self else { return }
-                output.send(.videoListDidChanged(videos: self.videos))
+            case .appendVideo(let url):
+                Task {
+                    guard let self else { return }
+                    let item = await self.makeVideoListItem(url: url)
+                    self.videoItems.append(item)
+                    self.output.send(.videoListDidChanged(videos: self.videoItems))
+                }
             }
         }
         .store(in: &cancellables)
@@ -56,33 +44,15 @@ extension MockVideoListViewModel: VideoListViewModel {
 }
 
 private extension MockVideoListViewModel {
-    /// 테스트를 위해 Dummy Video를 추가합니다.
-    func appendDummyVideo() {
-        let dummyVideo = VideoListItem(
-            title: "테스트_비디오.avi",
-            authorTitle: "from 지혜",
-            thumbnailImage: Data.imageData() ?? Data(),
-            duration: "1:30",
-            date: "2024.01.03"
+    /// 비디오에 해당하는 VideoListItem을 생성합니다. 실제 데이터가 아닌 Mock 값을 넣어줍니다.
+    func makeVideoListItem(url: URL) async -> VideoListItem {
+        return VideoListItem(
+            title: "테스트 타이틀.avi",
+            authorTitle: "지혜",
+            thumbnailImage: Data(),
+            videoURL: url,
+            duration: "2:30",
+            date: "2024.07.15"
         )
-        videos.append(dummyVideo)
-    }
-}
-
-// MARK: - Color Image Genenrator
-private extension Data {
-    static func imageData() -> Data? {
-        let color = UIColor.red
-        let size = CGSize(width: 100, height: 100)
-        UIGraphicsBeginImageContext(size)
-        color.setFill()
-        let rect = CGRect(origin: .zero, size: size)
-        UIRectFill(rect)
-
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        let imageData = image?.pngData()
-        return imageData
     }
 }
