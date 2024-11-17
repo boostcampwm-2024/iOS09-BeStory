336311a21a1a07cb2f2c48687c443ec27c5433d6
LURKS02 <thecrooks2@gmail.com>
2024-11-18 02:24:39 +0900

    [FEAT]: 동영상 썸네일 이미지를 생성하는 AVAsset Extension 메서드 추가

---
+++ Feature/Feature/Helper/AVAsset+Thumbnail.swift
@@ -0,0 +1,21 @@
+//
+//  AVAsset+GenerateThumbnails.swift
+//  Feature
+//
+//  Created by 디해 on 11/15/24.
+//
+
+import UIKit
+import AVFoundation
+
+extension AVAsset {
+    func generateThumbnail() async -> UIImage? {
+        let imageGenerator = AVAssetImageGenerator(asset: self)
+        imageGenerator.appliesPreferredTrackTransform = true
+        
+        let time = CMTime(seconds: 0.1, preferredTimescale: 600)
+        guard let cgImage = try? imageGenerator.copyCGImage(at: time, actualTime: nil) else { return nil }
+        let thumbnail = UIImage(cgImage: cgImage)
+        return thumbnail
+    }
+}
