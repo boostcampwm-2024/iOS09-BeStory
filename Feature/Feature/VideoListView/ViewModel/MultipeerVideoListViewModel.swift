774abcd8a0dda5f33bec7876aec65039c475ff44
LURKS02 <thecrooks2@gmail.com>
2024-11-18 02:28:34 +0900

    [FEAT]: 동영상을 기반으로 item을 생성하는 실제 ViewModel 구현
    - title: 파일 시스템 상에서의 파일 이름
    - authorTitle: 메타데이터 기반 사용자 이름
    - thumbnailImage: 동영상 썸네일
    - videoURL: 동영상 url
    - duration: 동영상 길이
    - date: 생성 날짜

--- Feature/Feature/VideoListView/ViewModel/MultipeerVideoListViewModel.swift
+++ Feature/Feature/VideoListView/ViewModel/MultipeerVideoListViewModel.swift
@@ -5,14 +5,16 @@
 //  Created by 디해 on 11/13/24.
 //
 
-import Foundation
+import AVFoundation
 import Combine
 
 public final class MultipeerVideoListViewModel {
-    private var videos: [VideoListItem] = []
+    private var videoItems: [VideoListItem] = []
     
     var output = PassthroughSubject<Output, Never>()
     var cancellables: Set<AnyCancellable> = []
+    
+    public init() { }
 }
 
 extension MultipeerVideoListViewModel: VideoListViewModel {
@@ -20,9 +22,17 @@ extension MultipeerVideoListViewModel: VideoListViewModel {
         input.sink { [weak self] input in
             switch input {
             case .viewDidLoad:
-                self?.output.send(.videoListDidChanged(videos: self?.videos ?? []))
-            case .appendVideo:
-                self?.appendVideoButtonTapped()
+                guard let self else { return }
+                output.send(.videoListDidChanged(videos: self.videoItems))
+            case .appendVideo(let url):
+                Task {
+                    guard let self else { return }
+                    let asset = AVAsset(url: url)
+                    // TODO: - 동영상 컨테이너에 업로드
+                    let item = await self.makeVideoListItem(with: url, asset: asset)
+                    self.videoItems.append(item)
+                    self.output.send(.videoListDidChanged(videos: self.videoItems))
+                }
             }
         }
         .store(in: &cancellables)
@@ -32,6 +42,33 @@ extension MultipeerVideoListViewModel: VideoListViewModel {
 }
 
 private extension MultipeerVideoListViewModel {
-    func appendVideoButtonTapped() {
+    func makeVideoListItem(with url: URL, asset: AVAsset) async -> VideoListItem {
+        let thumbnailImage = await asset.generateThumbnail()
+        let thumbnailData = thumbnailImage?.jpegData(compressionQuality: 0.8)
+        let durationString = convertToDurationString(with: await asset.totalSeconds)
+        let formattedDate = convertToFormattedDate(with: await asset.creationDate)
+        
+        return VideoListItem(
+            title: asset.title ?? "제목 없음",
+            authorTitle: await asset.author ?? "알 수 없음",
+            thumbnailImage: thumbnailData ?? Data(),
+            videoURL: url,
+            duration: durationString,
+            date: formattedDate
+        )
+    }
+    
+    func convertToDurationString(with totalSeconds: Float64?) -> String {
+        guard let totalSeconds else { return "--:--" }
+        let minutes = Int(totalSeconds) / 60
+        let seconds = Int(totalSeconds) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func convertToFormattedDate(with date: Date?) -> String {
+        guard let date else { return "알 수 없음" }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        return formatter.string(from: date)
     }
 }
