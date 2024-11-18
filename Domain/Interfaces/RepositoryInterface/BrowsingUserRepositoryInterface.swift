0a538a281e05b21a83d5dfe70b7bb359e357b5ed
Jung SeokYoung <thrdud0423@gmail.com>
2024-11-18 23:12:46 +0900

    [REFACT]: Repository 인터페이스에서 Socket 관련 의존성 삭제

--- Domain/Interfaces/RepositoryInterface/BrowsingUserRepositoryInterface.swift
+++ Domain/Interfaces/RepositoryInterface/BrowsingUserRepositoryInterface.swift
@@ -7,14 +7,11 @@
 
 import Combine
 import Entity
-import P2PSocket
 
 public protocol BrowsingUserRepositoryInterface {
 	var updatedBrowsingUser: PassthroughSubject<BrowsedUser, Never> { get }
 	var updatedInvitedUser: PassthroughSubject<InvitedUser, Never> { get }
 	var invitationReceived: PassthroughSubject<BrowsedUser, Never> { get }
-
-	init(socketProvider: SocketProvidable)
 	
 	func fetchBrowsingUsers() -> [BrowsedUser]
 	func inviteUser(with id: String, timeout: Double)
