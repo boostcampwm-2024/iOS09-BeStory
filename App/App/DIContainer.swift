6c29266a39968c83e5ff5f08921859007f9e5a2a
around-forest <kleedo19@naver.com>
2024-11-19 21:15:28 +0900

    [FEAT]: DIContainer에서 의존성을 주입하는 registerDependency 메서드 구현

--- App/App/DIContainer.swift
+++ App/App/DIContainer.swift
@@ -5,6 +5,12 @@
 //  Created by 이숲 on 11/19/24.
 //
 
+import Data
+import Feature
+import Interfaces
+import P2PSocket
+import UseCase
+
 final class DIContainer {
     static let shared = DIContainer()
 
@@ -21,5 +27,70 @@ final class DIContainer {
             fatalError("\(key) is not registered.")
         }
         return service
+    }
+
+    func registerDependency() {
+        registerP2PSocket()
+        registerRepository()
+        registerUseCase()
+        registerViewModel()
+    }
+}
+
+// MARK: - Dependency Register
+
+private extension DIContainer {
+    func registerP2PSocket() {
+        DIContainer.shared.register(
+            type: SocketProvidable.self,
+            instance: SocketProvider()
+        )
+    }
+
+    func registerRepository() {
+        DIContainer.shared.register(
+            type: BrowsingUserRepositoryInterface.self,
+            instance: BrowsingUserRepository(socketProvider: DIContainer.shared.resolve(type: SocketProvidable.self))
+        )
+
+        DIContainer.shared.register(
+            type: ConnectedUserRepositoryInterface.self,
+            instance: ConnectedUserRepository(socketProvider: DIContainer.shared.resolve(type: SocketProvidable.self))
+        )
+
+        DIContainer.shared.register(
+            type: SharingVideoRepositoryInterface.self,
+            instance: SharingVideoRepository(socketProvider: DIContainer.shared.resolve(type: SocketProvidable.self))
+        )
+
+    }
+
+    func registerUseCase() {
+        DIContainer.shared.register(
+            type: BrowsingUserUseCaseInterface.self,
+            instance: BrowsingUserUseCase(repository: DIContainer.shared.resolve(type: BrowsingUserRepositoryInterface.self))
+        )
+
+        DIContainer.shared.register(
+            type: ConnectedUserUseCaseInterface.self,
+            instance: ConnectedUserUseCase(repository: DIContainer.shared.resolve(type: ConnectedUserRepositoryInterface.self))
+        )
+    }
+
+    func registerViewModel() {
+        DIContainer.shared.register(
+            type: ConnectionViewModel.self,
+            instance: ConnectionViewModel(usecase: DIContainer.shared.resolve(type: BrowsingUserUseCaseInterface.self))
+        )
+
+        DIContainer.shared.register(
+            type: GroupInfoViewModel.self,
+            instance: GroupInfoViewModel()
+        )
+
+        DIContainer.shared.register(
+            type: MultipeerVideoListViewModel.self,
+            instance: MultipeerVideoListViewModel()
+        )
     }
 }
