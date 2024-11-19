58a6025b11ba126f8e5d567d0ddf7efbfd6e2173
around-forest <kleedo19@naver.com>
2024-11-19 20:51:51 +0900

    [FEAT]: DIContainer를 App Layer에 추가

---
+++ App/App/DIContainer.swift
@@ -0,0 +1,25 @@
+//
+//  DIContainer.swift
+//  App
+//
+//  Created by 이숲 on 11/19/24.
+//
+
+final class DIContainer {
+    static let shared = DIContainer()
+
+    private var services: [String: Any] = [:]
+
+    func register<T>(type: T.Type, instance: T) {
+        let key = String(describing: type)
+        services[key] = instance
+    }
+
+    func resolve<T>(type: T.Type) -> T {
+        let key = String(describing: type)
+        guard let service = services[key] as? T else {
+            fatalError("\(key) is not registered.")
+        }
+        return service
+    }
+}
