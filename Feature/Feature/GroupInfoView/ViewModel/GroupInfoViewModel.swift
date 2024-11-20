c9e136086a600c70c64ef3dcc8d282400eede208
051198Hz <peeper_o_o@naver.com>
2024-11-21 01:34:53 +0900

    [FEATURE]: GroupInfoViewModel과 유즈케이스를 연결

--- Feature/Feature/GroupInfoView/ViewModel/GroupInfoViewModel.swift
+++ Feature/Feature/GroupInfoView/ViewModel/GroupInfoViewModel.swift
@@ -6,26 +6,43 @@
 //
 
 import Combine
+import Entity
+import Interfaces
 
-public class GroupInfoViewModel {
+public final class GroupInfoViewModel {
     typealias Input = GroupInfoViewInput
     typealias Output = GroupInfoViewOutput
     
-    private var users = [InvitedUser]()
-    private var title: String = ""
+    private var users = [ConnectedUser]()
+    private var title: String
+    private let usecase: ConnectedUserUseCaseInterface
     
     var output = PassthroughSubject<GroupInfoViewOutput, Never>()
     var cancellables: Set<AnyCancellable> = []
 
-    public init() { }
+    public init(usecase: ConnectedUserUseCaseInterface, title: String) {
+        self.usecase = usecase
+        self.title = title
+        setupBind()
+    }
+    
+    public convenience init(usecase: ConnectedUserUseCaseInterface) {
+        let title = "차은우원빈현빈장원영의 이야기"
+        self.init(usecase: usecase, title: title)
+    }
 
     func transform(input: AnyPublisher<GroupInfoViewInput, Never>) -> AnyPublisher<GroupInfoViewOutput, Never> {
         input.sink { [weak self] inputResult in
+            guard let self else { return }
+            
             switch inputResult {
             case .viewDidLoad:
-                self?.output.send(.titleDidChanged(title: self?.title ?? ""))
+                output.send(.titleDidChanged(title: title))
+                usecase.fetchConnectedUsers().forEach { user in
+                    self.output.send(.userDidInvited(user: user))
+                }
             case .exitGroupButtonDidTab:
-                self?.exitGroupButtonDidTab()
+                exitGroupButtonDidTab()
             }
         }
         .store(in: &cancellables)
@@ -35,31 +52,45 @@ public class GroupInfoViewModel {
 }
 
 private extension GroupInfoViewModel {
-    func userStateDidChanged(user: InvitedUser) {
-        for index in users.indices {
-            guard users[index].id == user.id else { continue }
-            users[index] = user
+    func setupBind() {
+        usecase.updatedConnectedUser
+            .sink { [weak self] updatedUser in
+                guard let self else { return }
+                print(updatedUser)
+                switch updatedUser.state {
+                case .connected:
+                    userDidInvited(user: updatedUser)
+                case .disconnected:
+                    userDidExit(user: updatedUser)
+                case .pending:
+                    userStateDidChanged(user: updatedUser)
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func userStateDidChanged(user: ConnectedUser) {
+        if let userIndex = users.firstIndex(where: { $0.id == user.id }) {
+            guard users[userIndex].state != user.state else { return }
+            users[userIndex] = user
         }
         output.send(.userStateDidChanged(user: user))
     }
     
-    func userDidInvited(user: InvitedUser) {
+    func userDidInvited(user: ConnectedUser) {
+        guard !users.contains(user) else { return }
         users.append(user)
         output.send(.userDidInvited(user: user))
         output.send(.groupCountDidChanged(count: users.count))
     }
     
+    func userDidExit(user: ConnectedUser) {
+        if let userIndex = users.firstIndex(where: { $0.id == user.id }) {
+            users.remove(at: userIndex)
+        }
+        output.send(.userDidExit(user: user))
+        output.send(.groupCountDidChanged(count: users.count))
+    }
+    
     func exitGroupButtonDidTab() { }
-}
-
-enum GroupInfoViewInput {
-    case viewDidLoad
-    case exitGroupButtonDidTab
-}
-
-enum GroupInfoViewOutput {
-    case userStateDidChanged(user: InvitedUser)
-    case userDidInvited(user: InvitedUser)
-    case groupCountDidChanged(count: Int)
-    case titleDidChanged(title: String)
 }
