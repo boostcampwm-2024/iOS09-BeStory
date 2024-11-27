438a09c92f270fed315f74079952d3772cb9d40b
around-forest <kleedo19@naver.com>
2024-11-28 01:48:57 +0900

    [FIX]: actor로 동시성을 보장하도록 수정

--- Data/CRDT/LWWElementSet.swift
+++ Data/CRDT/LWWElementSet.swift
@@ -7,50 +7,50 @@
 
 import Foundation
 
-public final class LWWElementSet<T: Hashable> {
+public actor LWWElementSet<T: Hashable> {
     private var additions: [T: Timestamp] = [:]
     private var removals: [T: Timestamp] = [:]
-    private let queue = DispatchQueue(label: "crdt.queue", attributes: .concurrent)
 
     public func add(element: T, timestamp: Timestamp) {
-        queue.async(flags: .barrier) {
-            if let existingTimestamp = self.additions[element], existingTimestamp >= timestamp {
-                return
-            }
-            self.additions[element] = timestamp
+        if let existingTimestamp = self.additions[element], existingTimestamp >= timestamp {
+            return
         }
+        self.additions[element] = timestamp
+
     }
 
     public func remove(element: T, timestamp: Timestamp) {
-        queue.async(flags: .barrier) {
-            if let existingTimestamp = self.removals[element], existingTimestamp >= timestamp {
-                return
-            }
-            self.removals[element] = timestamp
+        if let existingTimestamp = self.removals[element], existingTimestamp >= timestamp {
+            return
         }
+        self.removals[element] = timestamp
+
     }
 
-    public func merge(with otherSet: LWWElementSet<T>) {
-        queue.async(flags: .barrier) {
-            for (element, timestamp) in otherSet.additions {
-                self.add(element: element, timestamp: timestamp)
-            }
-            for (element, timestamp) in otherSet.removals {
-                self.remove(element: element, timestamp: timestamp)
-            }
+    public func merge(with otherSet: LWWElementSet<T>) async {
+        let otherAdditions = await otherSet.additions
+        let otherRemovals = await otherSet.removals
+
+        for (element, timestamp) in otherAdditions {
+            self.add(element: element, timestamp: timestamp)
         }
+
+        for (element, timestamp) in otherRemovals {
+            self.remove(element: element, timestamp: timestamp)
+        }
+
     }
 
     public func elements() -> [T] {
         var result: [T] = []
-        queue.sync {
-            for (element, addTimestamp) in additions {
-                if let removeTimestamp = removals[element], removeTimestamp >= addTimestamp {
-                    continue
-                }
-                result.append(element)
+
+        for (element, addTimestamp) in additions {
+            if let removeTimestamp = removals[element], removeTimestamp >= addTimestamp {
+                continue
             }
+            result.append(element)
         }
+
         return result
     }
 }
