fa13c7df1cb73d6001a8f356721fc66756b6ac0d
LURKS02 <thecrooks2@gmail.com>
2024-11-28 14:00:36 +0900

    [REFACTOR]: FileSystemManager에서 해시 관련 코드 제거

--- Core/Utilities/FileSystemManager.swift
+++ Core/Utilities/FileSystemManager.swift
@@ -5,14 +5,13 @@
 //  Created by 디해 on 11/15/24.
 //
 
-import CryptoKit
 import Foundation
 
 public final class FileSystemManager {
     public static let shared = FileSystemManager()
-
+    public let folder: URL
+    
     private let fileManager = FileManager.default
-    private let folder: URL
     private let folderName: String = "videos"
     
     private init() {
@@ -46,26 +45,5 @@ public final class FileSystemManager {
         fileURLs.forEach { url in
             try? fileManager.removeItem(at: url)
         }
-    }
-    
-    public func collectHashes() -> [String: String] {
-        var fileHashes = [String: String]()
-        
-        guard let fileURLs = try? fileManager.contentsOfDirectory(
-            at: folder,
-            includingPropertiesForKeys: nil
-        ) else { return [:] }
-        for fileURL in fileURLs {
-            guard let hash = calculateFileHash(for: fileURL) else { return [:] }
-            fileHashes[fileURL.lastPathComponent] = hash
-        }
-        
-        return fileHashes
-    }
-    
-    private func calculateFileHash(for fileURL: URL) -> String? {
-        guard let fileData = try? Data(contentsOf: fileURL) else { return nil }
-        let hash = SHA256.hash(data: fileData)
-        return hash.compactMap { String(format: "%02x", $0) }.joined()
     }
 }
