7181f8218e1dd3fcfd59d239ff5f8567a3de16d6
around-forest <kleedo19@naver.com>
2024-11-29 01:06:40 +0900

    [FEAT]: SharedVideoEditViewController에 CollectionView 관련 기능 추가

--- Feature/Feature/SharedVideoEditView/SharedVideoEditViewController.swift
+++ Feature/Feature/SharedVideoEditView/SharedVideoEditViewController.swift
@@ -19,6 +19,8 @@ public final class SharedVideoEditViewController: UIViewController {
     // MARK: - UI Components
 
     private let videoPlayerView = VideoPlayerView()
+    private var videoTimelineCollectionView: UICollectionView!
+    private var videoTimelineDataSource: VideoTimelineDataSource!
 
     // MARK: - Initializer
 
@@ -36,10 +38,14 @@ public final class SharedVideoEditViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupViewAttributes()
         setupViewHierarchies()
         setupViewConstraints()
-        setupViewAttributes()
+
         setupViewBinding()
+
+        loadInitialData()
     }
     
     public override func viewWillAppear(_ animated: Bool) {
@@ -48,36 +54,12 @@ public final class SharedVideoEditViewController: UIViewController {
     }
 }
 
-// MARK: - UI Configure
+// MARK: - Binding
 
 private extension SharedVideoEditViewController {
-    enum Constants {
-        static let topMargin: CGFloat = 14
-        static let playerViewRatio: CGFloat = 9 / 16
-    }
-}
-
-private extension SharedVideoEditViewController {
-    func setupViewHierarchies() {
-        view.addSubview(videoPlayerView)
-    }
-    
-    func setupViewConstraints() {
-        videoPlayerView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(Constants.topMargin)
-            $0.leading.equalToSuperview()
-            $0.trailing.equalToSuperview()
-            $0.height.equalTo(videoPlayerView.snp.width).multipliedBy(Constants.playerViewRatio)
-        }
-    }
-    
-    func setupViewAttributes() {
-        view.backgroundColor = .black
-    }
-    
     func setupViewBinding() {
         let output = viewModel.transform(input.eraseToAnyPublisher())
-        
+
         output
             .receive(on: DispatchQueue.main)
             .sink { output in
@@ -85,3 +67,143 @@ private extension SharedVideoEditViewController {
             .store(in: &cancellables)
     }
 }
+
+// MARK: - UI Configure
+
+private extension SharedVideoEditViewController {
+    enum VideoPlayerViewConstants {
+        static let topMargin: CGFloat = 14
+        static let playerViewRatio: CGFloat = 9 / 16
+    }
+
+    enum VideoTimelineCollectionViewConstants {
+        static let backgroundColor = UIColor(
+            red: 31/255,
+            green: 41/255,
+            blue: 55/255,
+            alpha: 1
+        )
+        static let spacing: CGFloat = 10
+        static let topMargin: CGFloat = 14
+        static let height: CGFloat = 120
+        static let itemSize: CGSize = .init(width: 160, height: 90)
+    }
+
+    func setupViewAttributes() {
+        view.backgroundColor = .black
+        setupVideoTimelineCollectionView()
+    }
+
+    func setupVideoTimelineCollectionView() {
+        let layout = setupLayout()
+        videoTimelineCollectionView = .init(frame: .zero, collectionViewLayout: layout)
+
+        videoTimelineCollectionView.backgroundColor = VideoTimelineCollectionViewConstants.backgroundColor
+        videoTimelineCollectionView.showsHorizontalScrollIndicator = false
+        videoTimelineCollectionView.dragInteractionEnabled = true
+
+        let dataSource = setupDataSource()
+        self.videoTimelineDataSource = dataSource
+        videoTimelineCollectionView.dataSource = dataSource
+
+        videoTimelineCollectionView.register(
+            VideoTimelineCollectionViewCell.self,
+            forCellWithReuseIdentifier: VideoTimelineCollectionViewCell.reuseIdentifier
+        )
+    }
+
+    func setupViewHierarchies() {
+        [
+            videoPlayerView,
+            videoTimelineCollectionView
+        ].forEach({
+            view.addSubview($0)
+        })
+    }
+    
+    func setupViewConstraints() {
+        videoPlayerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(VideoPlayerViewConstants.topMargin)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(videoPlayerView.snp.width).multipliedBy(VideoPlayerViewConstants.playerViewRatio)
+        }
+
+        videoTimelineCollectionView.snp.makeConstraints {
+            $0.top.equalTo(videoPlayerView.snp.bottom).offset(VideoTimelineCollectionViewConstants.topMargin)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(VideoTimelineCollectionViewConstants.height)
+        }
+    }
+}
+
+// MARK: - UICollectionView Methods
+
+private extension SharedVideoEditViewController {
+    func setupLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = VideoTimelineCollectionViewConstants.spacing
+        layout.itemSize = VideoTimelineCollectionViewConstants.itemSize
+        return layout
+    }
+
+    func setupDataSource() -> VideoTimelineDataSource {
+        let dataSource = VideoTimelineDataSource(
+            collectionView: videoTimelineCollectionView
+        ) { collectionView, indexPath, item in
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: VideoTimelineCollectionViewCell.reuseIdentifier,
+                for: indexPath
+            )
+            guard let cell = cell as? VideoTimelineCollectionViewCell
+            else { return UICollectionViewCell() }
+            cell.configure(with: item)
+            return cell
+        }
+        return dataSource
+    }
+
+    func applySnapShot(with items: [VideoTimelineItem]) {
+        var snapshot = NSDiffableDataSourceSnapshot<VideoTimelineSection, VideoTimelineItem>()
+        snapshot.appendSections([.timeline])
+        snapshot.appendItems(items, toSection: .timeline)
+
+        videoTimelineDataSource.apply(snapshot, animatingDifferences: true)
+    }
+
+    func reload(with videos: [VideoTimelineItem]) {
+        applySnapShot(with: videos)
+    }
+
+    // TEST 용 메서드 입니다.
+    func loadInitialData() {
+        let items = [
+            VideoTimelineItem(
+                thumbnailImage: Data(),
+                duration: "0:0"
+            ),
+            VideoTimelineItem(
+                thumbnailImage: Data(),
+                duration: "10:0"
+            ),
+            VideoTimelineItem(
+                thumbnailImage: Data(),
+                duration: "20:0"
+            ),
+            VideoTimelineItem(
+                thumbnailImage: Data(),
+                duration: "30:0"
+            ),
+            VideoTimelineItem(
+                thumbnailImage: Data(),
+                duration: "40:0"
+            )
+        ]
+
+        applySnapShot(with: items)
+    }
+}
+
+// MARK: - UICollectionViewDelegate
