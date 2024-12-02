026bc77439b13f6ff6f9c428bf43b0cd8e647d6b
Yune gim <82448654+051198Hz@users.noreply.github.com>
2024-12-02 16:17:33 +0900

    [Feature/#68] VectorClock 구현 및 LWWElementSet에 적용 (#72)
    * [FEAT]: 벡터 클락 구현
    
    * [FEAT]: LWWElementSet에 VectorClock 적용
    
    * [FEAT]:  VectorClock을 머지 알고리즘에 적용 및 함수로 역할 분리
    
    * [REFACTOR]: 요소 반환 함수에 VectorClock적용
    
    * [FEAT]: LWWElement의 머지가 끝날 때 까지 기다리지 않아도 변화 이벤트를 받아볼 수 있는 updatedElements 추가
    
    * [FEAT]: 인코딩/디코딩을 위한 타입 LWWElementSetState 구현
    
    * [FEAT]: LWWElementSetState를 사용해 상태를 적용할 수 있도록 merge메서드 변경
    
    * [REFACTOR]: 컨벤션 일치를 위한 공백 수정
    
    * [FIX]: 이벤트의 순서가 섞여 도착할 때 합병하지 않는 이슈
    
    * [REFACTOR]: 컨벤션 일치
    
    * [FEAT]: 시계 카운터 조건문 명시성 개선
    
    * [TEST]: LWWElementSet 테스트 코드 추가
    
    * [CHORE]: VectorClock추가 및 이로 인한 TimeStamp 제거 관련 파일 변경사항 적용
    
    * [FIX]: 현재 상태가 변하지 않았음에도 병합 가능성을 검토하는 버그
    
    * [FIX]: 딕셔너리의 값을 업데이트 하지 않는 버그
    
    * [FIX]: 커스텀 Hashing 함수를 적용하기 위한 조건 부족
    
    * [TEST]: p0event를 합병하고 있던 테스트 코드를 p1event1를 합병하도록 수정
    
    * [REFACTOR]: 사용하지 않는 타입 채택 제거
    
    * [REFACTOR]: 알파벳순 import 컨벤션 준수를 위한 코드 수정
    
    * [REFACTOR]: 사용하지 않는 코드 삭제
    
    * [REFACTOR]: 사용하지 않는 코드 제거
    
    * [REFACTOR]: 컨벤션 준수를 위한 코드 수정
    
    * [REFACTOR]: 축약 가능한 코드를 축약
    
    * [REFACTOR]: 반환 타입 활용이 필수적임에 따른 어노테이션 제거
    
    * [REFACTOR]: 함수 위치 변경
    
    * [REFACTOR]: guard문으로 변경
    
    * [REFACTOR]: Typo 수정
    
    * [REFACTOR]: 고차함수 적용
    
    * [REFACTOR]: 동작을 더 잘 나타내기 위한 함수명 변경
    
    * [REFACTOR]: 함수명 변경에 따른 코드 수정
    
    * [REFACTOR]: 고차함수를 활용하도록 수정
    
    * [FIX]: guard문에서 > 연산자 버그로 롤백
    
    * [FIX]: 고차함수 로직 작성 실패로 롤백
    
    * [REFACTOR]: 접근제한자 수정
    
    * [REFACTOR]: 함수위치 이동
    
    * [FIX]: 고차함수 적용 실패로 롤백
    
    * [REFACTOR]: 반복문 실행 중 요소 삭제로 인한 인덱스 불일치 위험 제거
    
    * [FIX]: Entity에 대한 의존성이 중복으로 추가돼있는 버그

--- Data/CRDT/LWWElementSet.swift
+++ Data/CRDT/LWWElementSet.swift
@@ -5,49 +5,188 @@
 //  Created by 이숲 on 11/27/24.
 //
 
-import Foundation
+import Combine
 
-public actor LWWElementSet<T: Hashable> {
-    private var additions: [T: Timestamp] = [:]
-    private var removals: [T: Timestamp] = [:]
+public actor LWWElementSet<T: Codable & Hashable> {
+    private let id: Int
+    private var vectorClock: VectorClock
+    
+    private var additions: [T: VectorClock] = [:]
+    private var removals: [T: VectorClock] = [:]
+    private var waitSet: [LWWElementSet] = []
+    
+    public let updatedElements = PassthroughSubject<[T], Never>()
+    
+    public init(id: Int, peerCount: Int) {
+        self.id = id
+        vectorClock = VectorClock(replicaCount: peerCount)
+    }
+    
+    fileprivate init(
+        id: Int,
+        clock: VectorClock,
+        additions: [T: VectorClock],
+        removals: [T: VectorClock]
+    ) {
+        self.id = id
+        self.vectorClock = clock
+        self.additions = additions
+        self.removals = removals
+    }
+}
 
-    public func add(element: T, timestamp: Timestamp) {
-        if let existingTimestamp = self.additions[element], existingTimestamp >= timestamp {
-            return
-        }
-        self.additions[element] = timestamp
+public extension LWWElementSet {
+    func localAdd(element: T) -> LWWElementSetState<T> {
+        let clock = vectorClock.increase(replicaId: id)
+        additions.removeValue(forKey: element)
+        additions.updateValue(clock, forKey: element)
+        return payloading()
+    }
+    
+    func localRemove(element: T) -> LWWElementSetState<T> {
+        let clock = vectorClock.increase(replicaId: id)
+        removals.removeValue(forKey: element)
+        removals.updateValue(clock, forKey: element)
+        return payloading()
     }
 
-    public func remove(element: T, timestamp: Timestamp) {
-        if let existingTimestamp = self.removals[element], existingTimestamp >= timestamp {
-            return
+    func merge(with state: LWWElementSetState<T>) async {
+        let otherSet = state.excute()
+        if await !mergeAvailableSet(with: otherSet) {
+            return waitSet.append(otherSet)
         }
-        self.removals[element] = timestamp
+        return await mergeWatingSet()
     }
 
-    public func merge(with otherSet: LWWElementSet<T>) async {
-        let otherAdditions = await otherSet.additions
-        let otherRemovals = await otherSet.removals
-
-        for (element, timestamp) in otherAdditions {
-            self.add(element: element, timestamp: timestamp)
-        }
-
-        for (element, timestamp) in otherRemovals {
-            self.remove(element: element, timestamp: timestamp)
-        }
-    }
-
-    public func elements() -> [T] {
+    func elements() -> [T] {
         var result: [T] = []
-
-        for (element, addTimestamp) in additions {
-            if let removeTimestamp = removals[element], removeTimestamp >= addTimestamp {
+        
+        for (element, clock) in additions {
+            if let removeClock = removals[element], removeClock >= clock {
                 continue
             }
             result.append(element)
         }
-
         return result
     }
 }
+
+private extension LWWElementSet {
+    func mergeWatingSet() async {
+        var availableSet = [LWWElementSet]()
+        var completedSetIndexs = [Int]()
+        repeat {
+            while !availableSet.isEmpty {
+                let set = availableSet.removeLast()
+                await mergeAvailableSet(with: set)
+            }
+            for (index, set) in waitSet.enumerated()
+            where await vectorClock.isPaasable(to: set.vectorClock, replicaId: set.id) {
+                availableSet.append(set)
+                completedSetIndexs.append(index)
+            }
+            while !completedSetIndexs.isEmpty {
+                let index = completedSetIndexs.removeLast()
+                waitSet.remove(at: index)
+            }
+        } while !availableSet.isEmpty
+    }
+    
+    @discardableResult
+    func mergeAvailableSet(with otherSet: LWWElementSet<T>) async -> Bool {
+        let remoteClock = await otherSet.vectorClock
+        guard vectorClock.isPaasable(to: remoteClock, replicaId: otherSet.id) else { return false }
+        
+        let otherAdditions = await otherSet.additions
+        let otherRemovals = await otherSet.removals
+        
+        otherAdditions.forEach { add(element: $0, remoteClock: $1) }
+        otherRemovals.forEach { remove(element: $0, remoteClock: $1) }
+        
+        updatedElements.send(elements())
+        vectorClock.increase(replicaId: otherSet.id)
+        return true
+    }
+    
+    func add(element: T, remoteClock: VectorClock) {
+        if let clock = additions[element],
+              remoteClock <= clock
+        { return }
+        
+        additions.removeValue(forKey: element)
+        additions.updateValue(remoteClock, forKey: element)
+    }
+    
+    func remove(element: T, remoteClock: VectorClock) {
+        if let clock = removals[element],
+              remoteClock <= clock
+        { return }
+        
+        removals.removeValue(forKey: element)
+        removals.updateValue(remoteClock, forKey: element)
+    }
+    
+    func payloading() -> LWWElementSetState<T> {
+        return LWWElementSetState(
+            id: id,
+            clock: vectorClock,
+            additions: additions,
+            removals: removals
+        )
+    }
+}
+
+public struct LWWElementSetState <T: Codable & Hashable> {
+    fileprivate let id: Int
+    fileprivate var vectorClock: VectorClock
+    
+    fileprivate var additions: [T: VectorClock] = [:]
+    fileprivate var removals: [T: VectorClock] = [:]
+    
+    fileprivate init(
+        id: Int,
+        clock: VectorClock,
+        additions: [T: VectorClock],
+        removals: [T: VectorClock]
+    ) {
+        self.id = id
+        self.vectorClock = clock
+        self.additions = additions
+        self.removals = removals
+    }
+}
+
+fileprivate extension LWWElementSetState {
+    func excute() -> LWWElementSet<T> {
+        return LWWElementSet(
+            id: id,
+            clock: vectorClock,
+            additions: additions,
+            removals: removals
+        )
+    }
+}
+
+extension LWWElementSetState: Codable {
+    enum CodingKeys: CodingKey {
+        case id, vectorClock, additions, removals
+    }
+    
+    public init(from decoder: Decoder) throws{
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(Int.self, forKey: .id)
+        vectorClock = try values.decode(VectorClock.self, forKey: .vectorClock)
+        additions = try values.decode([T: VectorClock].self, forKey: .additions)
+        removals = try values.decode([T: VectorClock].self, forKey: .removals)
+    }
+    
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(vectorClock, forKey: .vectorClock)
+        try container.encode(additions, forKey: .additions)
+        try container.encode(removals, forKey: .removals)
+    }
+}
+
+extension LWWElementSetState: Equatable { }
