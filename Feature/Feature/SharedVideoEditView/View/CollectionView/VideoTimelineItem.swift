dd002cfecd7fb1d7e6ffaad73d7304532c29db9f
LURKS02 <thecrooks2@gmail.com>
2024-12-02 16:16:20 +0900

    [FEAT]: VideoTimeLineItem이 JSONString과 변환 가능하도록 구현

--- Feature/Feature/SharedVideoEditView/View/CollectionView/VideoTimelineItem.swift
+++ Feature/Feature/SharedVideoEditView/View/CollectionView/VideoTimelineItem.swift
@@ -7,12 +7,27 @@
 
 import Foundation
 
-public struct VideoTimelineItem: Hashable {
+public struct VideoTimelineItem: Codable, Hashable {
     let identifier = UUID()
     let thumbnailImage: Data
     let duration: String
 
     public func hash(into hasher: inout Hasher) {
         hasher.combine(identifier)
+    }
+}
+
+extension VideoTimelineItem {
+    static func fromJSONString(_ jsonString: String) -> VideoTimelineItem? {
+        let decoder = JSONDecoder()
+        guard let jsonData = jsonString.data(using: .utf8),
+              let item = try? decoder.decode(VideoTimelineItem.self, from: jsonData) else { return nil }
+        return item
+    }
+    
+    func toJSONString() -> String? {
+        let encoder = JSONEncoder()
+        guard let jsonData = try? encoder.encode(self) else { return nil }
+        return String(data: jsonData, encoding: .utf8)
     }
 }
