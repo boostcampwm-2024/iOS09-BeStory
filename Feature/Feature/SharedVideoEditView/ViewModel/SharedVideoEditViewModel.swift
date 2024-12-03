27a7b4746584b9e4044e01a61610a5f8a1f8001a
around-forest <kleedo19@naver.com>
2024-12-03 19:19:31 +0900

    [FEAT]: ViewModel에 테스트 코드 추가 및 수정

--- Feature/Feature/SharedVideoEditView/ViewModel/SharedVideoEditViewModel.swift
+++ Feature/Feature/SharedVideoEditView/ViewModel/SharedVideoEditViewModel.swift
@@ -8,6 +8,8 @@
 import AVFoundation
 import Combine
 import Core
+import Entity
+import Interfaces
 
 public final class SharedVideoEditViewModel {
     typealias Input = SharedVideoEditViewInput
@@ -16,38 +18,57 @@ public final class SharedVideoEditViewModel {
     private var output = PassthroughSubject<Output, Never>()
     private var cancellables: Set<AnyCancellable> = []
 
+    private var videoItems: [Video] = []
+    private var videoTimelineItems: [VideoTimelineItem] = []
+//    private let usecase: EditUseCaseInterface
+
     public init() {
-        setupBind()
+        self.setupBind()
     }
-    
+}
+
+// MARK: - Transform
+extension SharedVideoEditViewModel {
     func transform(_ input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
         input.sink(with: self) { owner, input in
             switch input {
             // 테스트 셋업으로 초기화합니다.
             case .setInitialState:
-                let initialItems = [
-                    VideoTimelineItem(
-                        thumbnailImage: Data(),
-                        duration: "0:0"
+                let videos = [
+                    Video(
+                        url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!,
+                        duration: 5,
+                        author: "test1",
+                        editor: ConnectedUser(id: "a", state: .connected, name: "a"),
+                        startTime: 0,
+                        endTime: 5
                     ),
-                    VideoTimelineItem(
-                        thumbnailImage: Data(),
-                        duration: "10:0"
+                    Video(
+                        url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")!,
+                        duration: 10,
+                        author: "test2",
+                        editor: ConnectedUser(id: "a", state: .connected, name: "a"),
+                        startTime: 0,
+                        endTime: 10
                     ),
-                    VideoTimelineItem(
-                        thumbnailImage: Data(),
-                        duration: "20:0"
-                    ),
-                    VideoTimelineItem(
-                        thumbnailImage: Data(),
-                        duration: "30:0"
-                    ),
-                    VideoTimelineItem(
-                        thumbnailImage: Data(),
-                        duration: "40:0"
+                    Video(
+                        url: URL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4")!,
+                        duration: 20,
+                        author: "test3",
+                        editor: ConnectedUser(id: "a", state: .connected, name: "a"),
+                        startTime: 0,
+                        endTime: 20
                     )
                 ]
-                owner.output.send(.timeLineDidChanged(items: initialItems))
+                
+                Task {
+                    for video in videos {
+                        let asset = AVAsset(url: video.url)
+                        await self.appendVideoTimelineItem(with: video.url, asset: asset)
+                    }
+                }
+
+//                owner.output.send(.timelineDidChanged(items: self.videoTimelineItems))
             }
         }
         .store(in: &cancellables)
@@ -56,10 +77,13 @@ public final class SharedVideoEditViewModel {
     }
 }
 
-// MARK: - Private Methods
+// MARK: - Binding
 private extension SharedVideoEditViewModel {
     func setupBind() { }
-	
+}
+
+// MARK: - Private Methods for Slider
+private extension SharedVideoEditViewModel {
 	/// `frameCount` 만큼의  frame Image들을 단일 frameImage로 리턴해줍니다.
 	func frameImage(from video: AVAsset, frameCount: Int) async -> UIImageWrapper? {
 		guard let cmTimeDuration = try? await video.load(.duration) else { return nil }
@@ -102,4 +126,32 @@ private extension SharedVideoEditViewModel {
 		
 		return .init(image: concatImage)
 	}
+}
+
+// MARK: - Private Methods for Timeline
+private extension SharedVideoEditViewModel {
+    func appendVideoTimelineItem(with url: URL, asset: AVAsset) async {
+        let item = await makeVideoTimelineItem(with: url, asset: asset)
+        videoTimelineItems.append(item)
+        output.send(.timelineDidChanged(items: videoTimelineItems))
+    }
+
+    func makeVideoTimelineItem(with url: URL, asset: AVAsset) async -> VideoTimelineItem {
+        let thumbnailImage = asset.generateThumbnail()
+        let thumbnailData = thumbnailImage?.jpegData(compressionQuality: 0.8)
+        let durationString = convertToDurationString(with: await asset.totalSeconds)
+
+        return VideoTimelineItem(
+            url: url,
+            thumbnailImage: thumbnailData ?? Data(),
+            duration: durationString
+        )
+    }
+
+    func convertToDurationString(with totalSeconds: Float64?) -> String {
+        guard let totalSeconds else { return "--:--" }
+        let minutes = Int(totalSeconds) / 60
+        let seconds = Int(totalSeconds) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 }
