e75704ec1627dd5fe6586db5671a95e208cf19e6
051198Hz <peeper_o_o@naver.com>
2024-12-04 23:38:09 +0900

    [FEAT]: 동영상 트랙간의 재생시간을 기준으로 다른 동영상을 가리지 않는 기능 추가

--- Feature/Feature/SharedVideoEditView/VideoMerger/VideoMerger.swift
+++ Feature/Feature/SharedVideoEditView/VideoMerger/VideoMerger.swift
@@ -8,4 +8,22 @@
 import AVFoundation
 import Entity
 
-enum VideoMerger { }
+enum VideoMerger {
+    private static func adoptAlphaInstruction(
+        _ layerInstruction: AVMutableVideoCompositionLayerInstruction,
+        currentTime: CMTime,
+        duration: CMTime
+    ) -> AVMutableVideoCompositionInstruction {
+        layerInstruction.setOpacity(0.0, at: .zero)
+        layerInstruction.setOpacity(1.0, at: currentTime)
+        
+        let endTime = CMTimeAdd(currentTime, duration)
+        layerInstruction.setOpacity(0.0, at: endTime)
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRange(start: currentTime, duration: duration)
+        instruction.layerInstructions = [layerInstruction]
+        
+        return instruction
+    }
+}
