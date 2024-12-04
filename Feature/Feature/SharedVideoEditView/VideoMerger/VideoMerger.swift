9126c3af683eceeb5e5a3301ec4fabe854d043d8
051198Hz <peeper_o_o@naver.com>
2024-12-04 23:44:17 +0900

    [FEAT]: 전달받은 동영상 목록과 해상도, 프레임 레이트로 하나의 동영상으로 합병하는 기능 추가

--- Feature/Feature/SharedVideoEditView/VideoMerger/VideoMerger.swift
+++ Feature/Feature/SharedVideoEditView/VideoMerger/VideoMerger.swift
@@ -9,6 +9,67 @@ import AVFoundation
 import Entity
 
 enum VideoMerger {
+    private enum Constants {
+        static let scaleSecond: Int32 = 600
+        static let defaultFrameRate: Int32 = 30
+    }
+
+    private static func merge(
+        _ videos: [Video],
+        size resultSize: CGSize,
+        frameRate: Int32 = Constants.defaultFrameRate
+    ) async throws -> (AVMutableComposition, AVMutableVideoComposition){
+        let composition = AVMutableComposition()
+        var currentTime = CMTime.zero
+        
+        var layerInstructions: [AVMutableVideoCompositionInstruction] = []
+        
+        for video in videos {
+            let asset = AVURLAsset(url: video.url)
+            
+            let assetVideoTrack = try await loadTrack(from: asset, withMediaType: .video)
+            let assetAudioTrack = try await loadTrack(from: asset, withMediaType: .audio)
+            
+            let startTime = CMTime(seconds: video.startTime, preferredTimescale: Constants.scaleSecond)
+            let duration = CMTime(seconds: video.endTime - video.startTime, preferredTimescale: Constants.scaleSecond)
+            let timeRange = CMTimeRange(start: startTime, duration: duration)
+            
+            let videoTrack = try await addTrack(to: composition, withMediaType: .video)
+            let audioTrack = try await addTrack(to: composition, withMediaType: .audio)
+            
+            try videoTrack.insertTimeRange(timeRange, of: assetVideoTrack, at: currentTime)
+            try audioTrack.insertTimeRange(timeRange, of: assetAudioTrack, at: currentTime)
+            
+            let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack)
+            let videoSize = try await assetVideoTrack.load(.naturalSize)
+            
+            let scale = scaleToAspectFit(with: videoSize, to: resultSize)
+            let center = moveToCenter(with: videoSize, to: resultSize, scale: scale)
+            
+            let transform = try await assetVideoTrack.load(.preferredTransform)
+                .concatenating(CGAffineTransform(scaleX: scale, y: scale))
+                .concatenating(CGAffineTransform(translationX: center.x, y: center.y))
+            layerInstruction.setTransform(transform, at: .zero)
+
+            let instruction = adoptAlphaInstruction(
+                layerInstruction,
+                currentTime: currentTime,
+                duration: duration
+            )
+            
+            layerInstructions.append(instruction)
+            currentTime = CMTimeAdd(currentTime, duration)
+        }
+        
+        // 비디오 컴포지션 설정
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.frameDuration = CMTime(value: 1, timescale: frameRate)
+        videoComposition.renderSize = resultSize
+        videoComposition.instructions =  layerInstructions
+        
+        return (composition, videoComposition)
+    }
+
     private static func export(
         composition: AVMutableComposition,
         videoComposition: AVMutableVideoComposition,
